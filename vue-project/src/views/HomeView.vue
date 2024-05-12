fa5fb200a78981c737809730f87eb99981447c22
Beinrain06 <elngoueni@gmail.com>
2024-05-12 16:23:23 -0400

    HomeView Page Card Mobile

--- vue-project/src/views/HomeView.vue
+++ vue-project/src/views/HomeView.vue
@@ -1,13 +1,13 @@
 <template>
   <main>
-    <div id="home_page" class="home_page w-full p-4">
+    <div id="home_page" class="home_page w-full px-0 py-2 xsm:p-4">
       <div
         v-for="(postItem, i) in dataPostsList"
         key="{{postItem.id}}"
-        class="card_out_wrapper max-w-full p-2"
+        class="card_out_wrapper max-w-full p-1 xsm:p-2"
       >
         <div class="card_in_wrapper">
-          <div class="card_img_wrap p-1">
+          <div class="card_img_wrap p-0 xsm:p-1">
             <img
               class="card_img object-cover w-full h-full p-1"
               :src="postItem.image"
@@ -18,13 +18,17 @@
             <div
               class="card_content_wrap relative w-full flex flex-col justify-center items-center rounded-sm gap-3"
             >
-              <div class="wrap_post_title w-full p-2">
+              <div class="wrap_post_title w-full px-2 py-1 sm:p-2">
                 <h1 class="post_title font-bold">{{ postItem.Title }}</h1>
               </div>
               <div class="author_info_wrapper w-full">
-                <div class="author_info_content max-w-6/12 flex gap-1 justify-center items-center">
-                  <h4 class="author_name text-base">{{ postItem.Author }} |</h4>
-                  <span class="posted_at font-light">{{ postItem.date }} |</span>
+                <div
+                  class="author_info_content max-w-6/12 flex flex-col xsm:flex-row gap-1 justify-center items-center text-center xsm:tex-left"
+                >
+                  <h4 class="author_name text-sm xsm:text-base">{{ postItem.Author }} |</h4>
+                  <span class="posted_at font-light text-sm md:text-base"
+                    >{{ postItem.date }} |</span
+                  >
                   <img
                     class="svg_edit w-6 h-4"
                     src="../assets/edit-svgrepo-com.svg"
@@ -32,7 +36,7 @@
                   />
                 </div>
               </div>
-              <div class="summary_wrapper w-full p-2">
+              <div class="summary_wrapper w-full px-2 py-4 sm:p-2">
                 <div class="summary_content">
                   <div class="w-full flex">
                     <h3 class="summary_title w-full font-medium">Summary</h3>
@@ -69,7 +73,7 @@ getClassify: () => {
 <style scoped>
 @media (min-width: 180px) {
   h1 {
-    font-size: calc(26px + 1.6vw);
+    font-size: calc(18px + 1.6vw);
     line-height: 1.2;
   }
 
@@ -81,9 +85,9 @@ getClassify: () => {
   p {
     font-family: 'Reddit Sans', sans-serif;
     font-optical-sizing: auto;
-    font-weight: 230;
+    font-weight: 390;
     font-style: normal;
-    font-size: calc(12px + 0.3vw);
+    font-size: calc(15px + 0.3vw);
     padding-top: 1rem;
     line-height: 1.6;
   }
@@ -91,7 +95,7 @@ getClassify: () => {
   .card_in_wrapper {
     perspective: 500px;
     width: 100%;
-    padding: 0.5rem;
+    padding: 0 0.25rem;
     display: grid;
     grid-auto-columns: 100%;
     grid-auto-rows: min-content max-content;
@@ -100,8 +104,8 @@ getClassify: () => {
 
   .card_in_wrapper .card_img_wrap {
     width: 100%;
-    height: 100%;
-    transform: scale(0.9);
+    height: 10rem;
+    transform: scale(1);
   }
 
   .card_in_wrapper .card_container {
@@ -127,10 +131,48 @@ getClassify: () => {
 
   .summary_content .paragraph_container {
     transform: scale(1);
+    line-height: 0.8;
+    letter-spacing: 1px;
+  }
+}
+
+@media (min-width: 240px) {
+  h1 {
+    font-size: calc(22px + 1.6vw);
+  }
+
+  p {
+    font-weight: 230;
+  }
+
+  .card_in_wrapper {
+    padding: 0.5rem;
+  }
+  .card_in_wrapper .card_img_wrap {
+    height: 14rem;
+  }
+
+  .summary_content .paragraph_container {
+    line-height: 1.2;
+    letter-spacing: 1px;
+  }
+}
+
+@media (min-width: 540px) {
+  h1 {
+    font-size: calc(25px + 1.6vw);
+  }
+
+  .card_in_wrapper .card_img_wrap {
+    height: 19.6rem;
   }
 }
 
 @media (min-width: 860px) {
+  h1 {
+    font-size: calc(26px + 1.6vw);
+  }
+
   h3 {
     font-size: calc(16px + 0.88vw);
     position: relative;
